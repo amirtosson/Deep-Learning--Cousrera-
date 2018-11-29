31caee50a89c4056b3b433fa37c69c6a99da3a90
Amir Tosson <45110899+amirtosson@users.noreply.github.com>
2018-11-29 19:32:06 +0100

    Add files via upload

---
+++ recurrent_NN.py
@@ -0,0 +1,223 @@
+import numpy as np
+from rnn_utils import *
+
+def rnn_cell_forward(xt, a_prev, parameters):   
+    # Retrieve parameters from "parameters"
+    Wax = parameters["Wax"]
+    Waa = parameters["Waa"]
+    Wya = parameters["Wya"]
+    ba = parameters["ba"]
+    by = parameters["by"]
+    
+
+    # compute next activation state using the formula given above
+    a_next = np.tanh(np.dot(Waa,a_prev)+ np.dot(Wax,xt)+ba)
+    # compute output of the current cell using the formula given above
+    yt_pred = softmax(np.dot(Wya,a_next)+by)   
+    
+    # store values you need for backward propagation in cache
+    cache = (a_next, a_prev, xt, parameters)
+    
+    return a_next, yt_pred, cache
+
+np.random.seed(1)
+xt = np.random.randn(3,10)
+a_prev = np.random.randn(5,10)
+Waa = np.random.randn(5,5)
+Wax = np.random.randn(5,3)
+Wya = np.random.randn(2,5)
+ba = np.random.randn(5,1)
+by = np.random.randn(2,1)
+parameters = {"Waa": Waa, "Wax": Wax, "Wya": Wya, "ba": ba, "by": by}
+
+a_next, yt_pred, cache = rnn_cell_forward(xt, a_prev, parameters)
+print("a_next[4] = ", a_next[4])
+print("a_next.shape = ", a_next.shape)
+print("yt_pred[1] =", yt_pred[1])
+print("yt_pred.shape = ", yt_pred.shape)
+
+def rnn_forward(x, a0, parameters):
+    # Initialize "caches" which will contain the list of all caches
+    caches = []
+    
+    # Retrieve dimensions from shapes of x and parameters["Wya"]
+    n_x, m, T_x = x.shape
+    n_y, n_a = parameters["Wya"].shape
+    
+    # initialize "a" and "y" with zeros (≈2 lines)
+     a = np.zeros([n_a, m, T_x])
+    y_pred = np.zeros([n_y, m, T_x])
+    
+    # Initialize a_next (≈1 line)
+    a_next = a0
+    
+    # loop over all time-steps
+    for t in range(T_x):
+        # Update next hidden state, compute the prediction, get the cache (≈1 line)
+        a_next, yt_pred, cache = rnn_cell_forward(x[:,:,t], a_next, parameters)
+        # Save the value of the new "next" hidden state in a (≈1 line)
+        a[:,:,t] = a_next
+        # Save the value of the prediction in y (≈1 line)
+        y_pred[:,:,t] = yt_pred
+        # Append "cache" to "caches" (≈1 line)
+        caches.append(cache)
+    
+    
+    # store values needed for backward propagation in cache
+    caches = (caches, x)
+    
+    return a, y_pred, caches
+
+np.random.seed(1)
+x = np.random.randn(3,10,4)
+a0 = np.random.randn(5,10)
+Waa = np.random.randn(5,5)
+Wax = np.random.randn(5,3)
+Wya = np.random.randn(2,5)
+ba = np.random.randn(5,1)
+by = np.random.randn(2,1)
+parameters = {"Waa": Waa, "Wax": Wax, "Wya": Wya, "ba": ba, "by": by}
+
+a, y_pred, caches = rnn_forward(x, a0, parameters)
+print("a[4][1] = ", a[4][1])
+print("a.shape = ", a.shape)
+print("y_pred[1][3] =", y_pred[1][3])
+print("y_pred.shape = ", y_pred.shape)
+print("caches[1][1][3] =", caches[1][1][3])
+print("len(caches) = ", len(caches))
+
+# GRADED FUNCTION: lstm_cell_forward
+
+def lstm_cell_forward(xt, a_prev, c_prev, parameters):
+ 
+
+    # Retrieve parameters from "parameters"
+    Wf = parameters["Wf"]
+    bf = parameters["bf"]
+    Wi = parameters["Wi"]
+    bi = parameters["bi"]
+    Wc = parameters["Wc"]
+    bc = parameters["bc"]
+    Wo = parameters["Wo"]
+    bo = parameters["bo"]
+    Wy = parameters["Wy"]
+    by = parameters["by"]
+    
+    # Retrieve dimensions from shapes of xt and Wy
+    n_x, m = xt.shape
+    n_y, n_a = Wy.shape
+
+ 
+    # Concatenate a_prev and xt (≈3 lines)
+    concat = np.zeros([n_x+n_a,m])
+    concat[: n_a, :] = a_prev
+    concat[n_a :, :] = xt
+
+    # Compute values for ft, it, cct, c_next, ot, a_next using the formulas given figure (4) (≈6 lines)
+    ft = sigmoid(np.dot(Wf,concat)+bf)
+    it = sigmoid(np.dot(Wi,concat)+bi)
+    cct = np.tanh(np.dot(Wc,concat)+bc)
+    c_next = ft*c_prev+it*cct
+    ot = sigmoid(np.dot(Wo,concat)+bo)
+    a_next = ot*np.tanh(c_next)
+    
+    # Compute prediction of the LSTM cell (≈1 line)
+    yt_pred = softmax(c_next)
+ 
+
+    # store values needed for backward propagation in cache
+    cache = (a_next, c_next, a_prev, c_prev, ft, it, cct, ot, xt, parameters)
+
+    return a_next, c_next, yt_pred, cache
+np.random.seed(1)
+xt = np.random.randn(3,10)
+a_prev = np.random.randn(5,10)
+c_prev = np.random.randn(5,10)
+Wf = np.random.randn(5, 5+3)
+bf = np.random.randn(5,1)
+Wi = np.random.randn(5, 5+3)
+bi = np.random.randn(5,1)
+Wo = np.random.randn(5, 5+3)
+bo = np.random.randn(5,1)
+Wc = np.random.randn(5, 5+3)
+bc = np.random.randn(5,1)
+Wy = np.random.randn(2,5)
+by = np.random.randn(2,1)
+
+parameters = {"Wf": Wf, "Wi": Wi, "Wo": Wo, "Wc": Wc, "Wy": Wy, "bf": bf, "bi": bi, "bo": bo, "bc": bc, "by": by}
+
+a_next, c_next, yt, cache = lstm_cell_forward(xt, a_prev, c_prev, parameters)
+print("a_next[4] = ", a_next[4])
+print("a_next.shape = ", c_next.shape)
+print("c_next[2] = ", c_next[2])
+print("c_next.shape = ", c_next.shape)
+print("yt[1] =", yt[1])
+print("yt.shape = ", yt.shape)
+print("cache[1][3] =", cache[1][3])
+print("len(cache) = ", len(cache))
+
+
+# GRADED FUNCTION: lstm_forward
+
+def lstm_forward(x, a0, parameters):
+
+    # Initialize "caches", which will track the list of all the caches
+    caches = []
+    
+ 
+    # Retrieve dimensions from shapes of x and parameters['Wy'] (≈2 lines)
+    n_x, m, T_x = x.shape
+    n_y, n_a = parameters['Wy'].shape
+    
+    # initialize "a", "c" and "y" with zeros (≈3 lines)
+    a = np.zeros([n_a,m,T_x])
+    c = np.zeros([n_a,m,T_x])
+    y = np.zeros([n_y,m,T_x])
+    
+    # Initialize a_next and c_next (≈2 lines)
+    a_next = a0
+    c_next = np.zeros([n_a,m])
+    
+    # loop over all time-steps
+    for t in range(T_x):
+        # Update next hidden state, next memory state, compute the prediction, get the cache (≈1 line)
+        a_next, c_next, yt, cache = lstm_cell_forward(x[:,:,t], a_next, c_next, parameters)
+        # Save the value of the new "next" hidden state in a (≈1 line)
+        a[:,:,t] = a_next
+        # Save the value of the prediction in y (≈1 line)
+        y[:,:,t] = yt
+        # Save the value of the next cell state (≈1 line)
+        c[:,:,t]  = c_next
+        # Append the cache into caches (≈1 line)
+        caches.append(cache)
+
+    
+    # store values needed for backward propagation in cache
+    caches = (caches, x)
+
+    return a, y, c, caches
+np.random.seed(1)
+x = np.random.randn(3,10,7)
+a0 = np.random.randn(5,10)
+Wf = np.random.randn(5, 5+3)
+bf = np.random.randn(5,1)
+Wi = np.random.randn(5, 5+3)
+bi = np.random.randn(5,1)
+Wo = np.random.randn(5, 5+3)
+bo = np.random.randn(5,1)
+Wc = np.random.randn(5, 5+3)
+bc = np.random.randn(5,1)
+Wy = np.random.randn(2,5)
+by = np.random.randn(2,1)
+
+parameters = {"Wf": Wf, "Wi": Wi, "Wo": Wo, "Wc": Wc, "Wy": Wy, "bf": bf, "bi": bi, "bo": bo, "bc": bc, "by": by}
+
+a, y, c, caches = lstm_forward(x, a0, parameters)
+print("a[4][3][6] = ", a[4][3][6])
+print("a.shape = ", a.shape)
+print("y[1][4][3] =", y[1][4][3])
+print("y.shape = ", y.shape)
+print("caches[1][1[1]] =", caches[1][1][1])
+print("c[1][2][1]", c[1][2][1])
+print("len(caches) = ", len(caches))
+
